e4f33772c5287939ef2f54f81dbcfecd19e05eba
ASHIK AHMED <ashikahmed121@gmail.com>
2023-03-13 23:22:15 +0600

    FreedomFighter Model again changed to officialRank and freedomFighterRank type to object

--- models/FreedomFighter.js
+++ models/FreedomFighter.js
@@ -4,10 +4,6 @@ const { ObjectId } = mongoose.Schema.Types;
 
 
 const freedomFighterSchema = mongoose.Schema({
-    memberType: {
-        type: String,
-        required: true
-    },
     fullName: {
         type: String,
         required: [true, 'Name is required'],
@@ -15,7 +11,10 @@ const freedomFighterSchema = mongoose.Schema({
         minLength: [3, 'Name must be at least 3 characters'],
         maxLength: [100, 'Name length  is too large'],
     },
-
+    category: {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
         required: [true, "Email is required"],
@@ -63,78 +62,77 @@ const freedomFighterSchema = mongoose.Schema({
     },
 
     officialRank: {
-        type: Object,
-        // rank: {
-        //     type: String,
-        //     required: [true, "Official Rank is required"],
-        //     minLength: [3, "Designation must be at least 3 characters long"],
-        //     enum: ['General',
-        //         'Lieutenant general',
-        //         'Major general',
-        //         'Brigadier general',
-        //         'Colonel',
-        //         'Lieutenant colonel',
-        //         'Major',
-        //         'Captain',
-        //         'Lieutenant',
-        //         'Second lieutenant',
-        //         'Officer cadet',
-        //         'Master warrant officer',
-        //         'Senior warrant officer',
-        //         'Warrant officer',
-        //         'Regiment Sergeant Major',
-        //         'Quarter Master Sergeant',
-        //         'Sergeant Major',
-        //         'Master Sergeant',
-        //         'Sergeant',
-        //         'Corporal',
-        //         'Lance corporal',
-        //         'Sainik',
-        //         'Admiral',
-        //         'Vice admiral',
-        //         'Rear admiral',
-        //         'Commodore',
-        //         'Captain',
-        //         'Commander',
-        //         'Lieutenant commander',
-        //         'Lieutenant',
-        //         'Sub-lieutenant',
-        //         'Acting sub-lieutenant',
-        //         'Midshipman',
-        //         'Officer cadet',
-        //         'Master chief petty officer',
-        //         'Senior chief petty officer',
-        //         'Chief petty officer',
-        //         'Leading seaman',
-        //         'Able seaman',
-        //         'Ordinary seaman',
-        //         'Air Chief Marshal',
-        //         'Air Marshal',
-        //         'Air Vice-Marshal',
-        //         'Air Commodore',
-        //         'Group Captain',
-        //         'Wing Commander',
-        //         'Squadron Leader',
-        //         'Flight Lieutenant',
-        //         'Flight Sergeant',
-        //         'Flying Officer',
-        //         'Pilot Officer',
-        //         'Officer cadet',
-        //         'Master warrant officer',
-        //         'Senior warrant officer',
-        //         'Warrant officer',
-        //         'Sergeant',
-        //         'Corporal',
-        //         'Leading aircraftman',
-        //         'Aircraftman 1',
-        //         'Aircraftman 2'
-        //     ],
-        // },
-        // point: {
-        //     type: Number,
-        //     required: [true, "Official rank point is required"],
-        //     min: [0, "Official Rank point cant be negative"]
-        // }
+        rank: {
+            type: String,
+            required: [true, "Official Rank is required"],
+            minLength: [3, "Designation must be at least 3 characters long"],
+            enum: ['General',
+                'Lieutenant general',
+                'Major general',
+                'Brigadier general',
+                'Colonel',
+                'Lieutenant colonel',
+                'Major',
+                'Captain',
+                'Lieutenant',
+                'Second lieutenant',
+                'Officer cadet',
+                'Master warrant officer',
+                'Senior warrant officer',
+                'Warrant officer',
+                'Regiment Sergeant Major',
+                'Quarter Master Sergeant',
+                'Sergeant Major',
+                'Master Sergeant',
+                'Sergeant',
+                'Corporal',
+                'Lance corporal',
+                'Sainik',
+                'Admiral',
+                'Vice admiral',
+                'Rear admiral',
+                'Commodore',
+                'Captain',
+                'Commander',
+                'Lieutenant commander',
+                'Lieutenant',
+                'Sub-lieutenant',
+                'Acting sub-lieutenant',
+                'Midshipman',
+                'Officer cadet',
+                'Master chief petty officer',
+                'Senior chief petty officer',
+                'Chief petty officer',
+                'Leading seaman',
+                'Able seaman',
+                'Ordinary seaman',
+                'Air Chief Marshal',
+                'Air Marshal',
+                'Air Vice-Marshal',
+                'Air Commodore',
+                'Group Captain',
+                'Wing Commander',
+                'Squadron Leader',
+                'Flight Lieutenant',
+                'Flight Sergeant',
+                'Flying Officer',
+                'Pilot Officer',
+                'Officer cadet',
+                'Master warrant officer',
+                'Senior warrant officer',
+                'Warrant officer',
+                'Sergeant',
+                'Corporal',
+                'Leading aircraftman',
+                'Aircraftman 1',
+                'Aircraftman 2'
+            ],
+        },
+        point: {
+            type: Number,
+            required: [true, "Official rank point is required"],
+            min: [0, "Official Rank point cant be negative"]
+        }
     },
 
     birthday: {
@@ -142,20 +140,19 @@ const freedomFighterSchema = mongoose.Schema({
     },
 
     freedomFighterRank: {
-        type: Object,
-        // rank: {
-        //     type: String,
-        //     required: [true, "Freedom Fighter rank is required"],
-        //     minLength: [3, "Designation must be at least 3 characters long"],
-        //     enum: ["Bir Shreshtho", "Bir Bikrom", "Bir Uttam", "Bir Protik", "Bir Muktijoddha"]
-        // },
+        rank: {
+            type: String,
+            required: [true, "Freedom Fighter rank is required"],
+            minLength: [3, "Designation must be at least 3 characters long"],
+            enum: ["Bir Shreshtho", "Bir Bikrom", "Bir Uttam", "Bir Protik", "Bir Muktijoddha"]
+        },
 
-        // point: {
-        //     type: Number,
-        //     required: [true, "Freedom Fighter rank point is required"],
-        //     min: [0, "Freedom Fighter Rank point cant be negative"]
+        point: {
+            type: Number,
+            required: [true, "Freedom Fighter rank point is required"],
+            min: [0, "Freedom Fighter Rank point cant be negative"]
 
-        // }
+        }
 
     },
 

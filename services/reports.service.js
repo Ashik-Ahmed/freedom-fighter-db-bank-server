f088fec63d78990ca80c3778c1f6df0d105db612
ASHIK AHMED <ashikahmed121@gmail.com>
2023-05-21 14:43:57 +0600

    getting matched data from mongodb

--- services/reports.service.js
+++ services/reports.service.js
@@ -4,12 +4,31 @@ const FreedomFighter = require("../models/FreedomFighter");
 exports.clearanceReportService = async (data) => {
     console.log('report service');
     const { event, year } = data
-    console.log(event, year);
+    console.log(event, year, typeof (year));
 
-    // const result = await FreedomFighter.aggregate([
-    //     {
-    //         $match:
-    //     }
-    // ])
+    const result = await FreedomFighter.aggregate([
+        {
+            $match: {
+                primarySelection: {
+                    $exists: true,
+                    $all: [
+                        {
+                            $elemMatch: {
+                                event: event,
+                                year: year
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            $project: {
+                primarySelection: 1
+            }
+        }
+    ])
+    // console.log(result);
+    return result.length;
 
 }

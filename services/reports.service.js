ca2ec02da3d6c2458e8f9f2cc7f6e31808cb4339
ASHIK AHMED <ashikahmed121@gmail.com>
2023-05-21 17:30:05 +0600

    generated total sent count as per rank

--- services/reports.service.js
+++ services/reports.service.js
@@ -35,6 +35,46 @@ exports.clearanceReportService = async (data) => {
         }
     ])
     // console.log(result);
+
+    const reportData = [];
+    result.map(force => {
+        // console.log(force._id);
+        // console.log(force.members.length);
+        // const forceData = {};
+        const totalAliveOfficerSent = force.members.filter(member => {
+            if (member.status == 'Alive' && member.officialRank.point > 13) {
+                return member;
+            }
+        });
+        const totalAliveORSent = force.members.filter(member => {
+            if (member.status == 'Alive' && member.officialRank.point < 13) {
+                return member;
+            }
+        });
+        const totalDeadOfficerSent = force.members.filter(member => {
+            if (member.status == 'Dead' && member.officialRank.point > 13) {
+                return member;
+            }
+        });
+        const totalDeadORSent = force.members.filter(member => {
+            if (member.status == 'Dead' && member.officialRank.point < 13) {
+                return member;
+            }
+        });
+        // console.log('Alive Officer:', totalAliveOfficerSent);
+        let forceData = {
+            force: force._id,
+            totalAliveOfficer: totalAliveOfficerSent.length,
+            totalAliveOR: totalAliveORSent.length,
+            totalDeadOfficer: totalDeadOfficerSent.length,
+            totalDeadOR: totalDeadORSent.length
+        }
+        console.log('forceData:', forceData);
+        reportData.push(forceData)
+
+    })
+    console.log('forceData:', forceData);
+
     return result;
 
 }

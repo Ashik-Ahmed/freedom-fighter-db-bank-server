436e6ae76cd7feb8e07bfc4758120ce0e0ae05a4
ASHIK AHMED <ashikahmed121@gmail.com>
2023-05-22 21:38:06 +0600

    alive Officer approved count calculated

--- services/reports.service.js
+++ services/reports.service.js
@@ -45,6 +45,7 @@ exports.clearanceReportService = async (data) => {
             if (member.status == 'Alive' && member.officialRank.point > 13) {
                 return member;
             }
+            console.log(member);
         });
         const totalAliveORSent = force.members.filter(member => {
             if (member.status == 'Alive' && member.officialRank.point < 13) {
@@ -61,20 +62,50 @@ exports.clearanceReportService = async (data) => {
                 return member;
             }
         });
+
+
+        // Category wise approved count 
+        const aliveOfficerApproved = totalAliveOfficerSent.filter(aliveOfficer => {
+            return aliveOfficer.primarySelection.some(eventData => eventData.event == event && eventData.year == year && eventData.verificationStatus.status == 'Success')
+        })
+        console.log('totalAliveApproved:', aliveOfficerApproved.length);
+        // const aliveORApproved = totalAliveORSent.filter(aliveOR => {
+        //     if (aliveOR.primarySelection.verificationStatus.status == 'Success') {
+        //         return aliveOR
+        //     }
+        // })
+        // const deadOfficerApproved = totalDeadOfficerSent.filter(deadOfficer => {
+        //     if (deadOfficer.primarySelection.verificationStatus.status == 'Success') {
+        //         return deadOfficer
+        //     }
+        // })
+        // const deadORApproved = totalDeadORSent.filter(deadOR => {
+        //     if (deadOR.primarySelection.verificationStatus.status == 'Success') {
+        //         return deadOR
+        //     }
+        // })
+
+
         // console.log('Alive Officer:', totalAliveOfficerSent);
         let forceData = {
             force: force._id,
             totalAliveOfficer: totalAliveOfficerSent.length,
+            // totalAliveOfficerApproved: totalAliveOfficerApproved.length,
+            // totalAliveOfficerReject: totalAliveOfficerSent.length - totalAliveOfficerApproved.length,
             totalAliveOR: totalAliveORSent.length,
             totalDeadOfficer: totalDeadOfficerSent.length,
-            totalDeadOR: totalDeadORSent.length
+            totalDeadOR: totalDeadORSent.length,
+            aliveOfficerApproved: aliveOfficerApproved,
+            // aliveORApproved: aliveORApproved.length,
+            // deadOfficerApproved: deadOfficerApproved.length,
+            // deadORApproved: deadORApproved.length
         }
-        console.log('forceData:', forceData);
+        // console.log('forceData:', forceData);
         reportData.push(forceData)
 
     })
-    console.log('forceData:', forceData);
+    // console.log('forceData:', reportData);
 
-    return result;
+    return reportData;
 
 }

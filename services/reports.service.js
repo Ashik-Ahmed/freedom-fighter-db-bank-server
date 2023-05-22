20b94cbc1da48f5f0dadaebfc58c65e1c792927a
ASHIK AHMED <ashikahmed121@gmail.com>
2023-05-22 23:11:51 +0600

    previous year report generated

--- services/reports.service.js
+++ services/reports.service.js
@@ -4,9 +4,11 @@ const FreedomFighter = require("../models/FreedomFighter");
 exports.clearanceReportService = async (data) => {
     console.log('report service');
     const { event, year } = data
-    console.log(event, year, typeof (year));
 
-    const result = await FreedomFighter.aggregate([
+    const currentReport = [];
+    const previousYearReport = []
+
+    const members = await FreedomFighter.aggregate([
         {
             $match: {
                 primarySelection: {
@@ -36,8 +38,8 @@ exports.clearanceReportService = async (data) => {
     ])
     // console.log(result);
 
-    const reportData = [];
-    result.map(force => {
+
+    members.map(force => {
         // console.log(force._id);
         // console.log(force.members.length);
         // const forceData = {};
@@ -45,7 +47,7 @@ exports.clearanceReportService = async (data) => {
             if (member.status == 'Alive' && member.officialRank.point > 13) {
                 return member;
             }
-            console.log(member);
+            // console.log(member);
         });
         const totalAliveORSent = force.members.filter(member => {
             if (member.status == 'Alive' && member.officialRank.point < 13) {
@@ -110,11 +112,95 @@ exports.clearanceReportService = async (data) => {
             deadORApproved: deadORApproved.length
         }
         // console.log('forceData:', forceData);
-        reportData.push(forceData)
+        currentReport.push(forceData)
 
     })
     // console.log('forceData:', reportData);
 
-    return reportData;
+    // previous year report calculation 
+    const previousYearInvitees = await FreedomFighter.aggregate([
+        {
+            $match: {
+                primarySelection: {
+                    $exists: true,
+                    $all: [
+                        {
+                            $elemMatch: {
+                                event: event,
+                                year: (year - 1).toString()
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            $project: {
+                name: 1, force: 1
+            }
+        },
+        {
+            $group: {
+                _id: "$force",
+                members: { $push: "$$ROOT" }
+            }
+        }
+    ])
+    console.log('previous year invited: ', previousYearInvitees.length);
+
+    previousYearInvitees.map(force => {
+        const totalAliveOfficerSent = force.members.filter(member => {
+            if (member.status == 'Alive' && member.officialRank.point > 13) {
+                return member;
+            }
+            // console.log(member);
+        });
+        const totalAliveORSent = force.members.filter(member => {
+            if (member.status == 'Alive' && member.officialRank.point < 13) {
+                return member;
+            }
+        });
+        const totalDeadOfficerSent = force.members.filter(member => {
+            if (member.status == 'Dead' && member.officialRank.point > 13) {
+                return member;
+            }
+        });
+        const totalDeadORSent = force.members.filter(member => {
+            if (member.status == 'Dead' && member.officialRank.point < 13) {
+                return member;
+            }
+        });
+
+
+        // Category wise approved count 
+        const aliveOfficerApproved = totalAliveOfficerSent.filter(aliveOfficer => {
+            return aliveOfficer.primarySelection.some(eventData => eventData.event == event && eventData.year == (year - 1) && eventData.verificationStatus.status == 'Success')
+        })
+        const aliveORApproved = totalAliveORSent.filter(aliveOR => {
+            return aliveOR.primarySelection.some(eventData => eventData.event == event && eventData.year == (year - 1) && eventData.verificationStatus.status == 'Success')
+        })
+        const deadOfficerApproved = totalDeadOfficerSent.filter(deadOfficer => {
+            return deadOfficer.primarySelection.some(eventData => eventData.event == event && eventData.year == (year - 1) && eventData.verificationStatus.status == 'Success')
+        })
+        const deadORApproved = totalDeadORSent.filter(deadOR => {
+            return deadOR.primarySelection.some(eventData => eventData.event == event && eventData.year == (year - 1) && eventData.verificationStatus.status == 'Success')
+        })
+
+        let forceData = {
+            force: force._id,
+            totalAliveOfficer: totalAliveOfficerSent.length,
+            totalAliveOR: totalAliveORSent.length,
+            totalDeadOfficer: totalDeadOfficerSent.length,
+            totalDeadOR: totalDeadORSent.length,
+            aliveOfficerApproved: aliveOfficerApproved.length,
+            aliveORApproved: aliveORApproved.length,
+            deadOfficerApproved: deadOfficerApproved.length,
+            deadORApproved: deadORApproved.length
+        }
+        previousYearReport.push(forceData)
+    })
+
+    const fullReport = { currentReport, previousYearReport }
+    return fullReport;
 
 }

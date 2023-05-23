09638bd17f6ab571b424781f2dcc70c7d85c283c
ASHIK AHMED <ashikahmed121@gmail.com>
2023-05-23 23:13:22 +0600

    result merged for current report and previous year report

--- services/report.test.js
+++ services/report.test.js
@@ -92,7 +92,7 @@ exports.getReportService = async (data) => {
         {
             $group: {
                 _id: "$force",
-                aliveOfficer: {
+                previousAliveOfficer: {
                     $sum: {
                         $cond: [
                             { $and: [{ $eq: ["$status", "Alive"] }, { $gt: ["$officialRank.point", 13] }] },
@@ -101,7 +101,7 @@ exports.getReportService = async (data) => {
                         ]
                     }
                 },
-                aliveJCO: {
+                previousAliveJCO: {
                     $sum: {
                         $cond: [
                             { $and: [{ $eq: ["$status", "Alive"] }, { $lte: ["$officialRank.point", 13] }] },
@@ -110,7 +110,7 @@ exports.getReportService = async (data) => {
                         ]
                     }
                 },
-                deadOfficer: {
+                previousDeadOfficer: {
                     $sum: {
                         $cond: [
                             { $and: [{ $eq: ["$status", "Dead"] }, { $gt: ["$officialRank.point", 13] }] },
@@ -119,7 +119,7 @@ exports.getReportService = async (data) => {
                         ]
                     }
                 },
-                deadJCO: {
+                previousDeadJCO: {
                     $sum: {
                         $cond: [
                             { $and: [{ $eq: ["$status", "Dead"] }, { $lte: ["$officialRank.point", 13] }] },
@@ -134,14 +134,30 @@ exports.getReportService = async (data) => {
             $project: {
                 _id: 0,
                 force: "$_id",
-                aliveOfficer: 1,
-                aliveJCO: 1,
-                deadOfficer: 1,
-                deadJCO: 1
+                previousAliveOfficer: 1,
+                previousAliveJCO: 1,
+                previousDeadOfficer: 1,
+                previousDeadJCO: 1
             }
         }
     ])
     // console.log(result.length);
     const reportData = [...result, ...previousYearResult]
-    return reportData;
+
+    const mergedObjects = reportData.reduce((result, obj) => {
+        const force = obj.force;
+        const existingObj = result.find(item => item.force === force);
+
+        if (existingObj) {
+            // Merge the current object with the existing object
+            Object.assign(existingObj, obj);
+        } else {
+            // Add the current object as a new entry
+            result.push(obj);
+        }
+
+        return result;
+    }, []);
+    console.log(mergedObjects);
+    return mergedObjects;
 }

982510aee6f7a4b0eb0d24233ca1b0ba6f32c5b1
ASHIK AHMED <ashikahmed121@gmail.com>
2023-05-23 22:43:42 +0600

    previous year report generated and combined with current data

--- services/report.test.js
+++ services/report.test.js
@@ -72,6 +72,76 @@ exports.getReportService = async (data) => {
             }
         }
     ])
-    console.log(result.length);
-    return result;
+
+    const previousYearResult = await FreedomFighter.aggregate([
+        {
+            $match: {
+                primarySelection: {
+                    $exists: true,
+                    $all: [
+                        {
+                            $elemMatch: {
+                                event: event,
+                                year: (year - 1).toString()
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            $group: {
+                _id: "$force",
+                aliveOfficer: {
+                    $sum: {
+                        $cond: [
+                            { $and: [{ $eq: ["$status", "Alive"] }, { $gt: ["$officialRank.point", 13] }] },
+                            1,
+                            0
+                        ]
+                    }
+                },
+                aliveJCO: {
+                    $sum: {
+                        $cond: [
+                            { $and: [{ $eq: ["$status", "Alive"] }, { $lte: ["$officialRank.point", 13] }] },
+                            1,
+                            0
+                        ]
+                    }
+                },
+                deadOfficer: {
+                    $sum: {
+                        $cond: [
+                            { $and: [{ $eq: ["$status", "Dead"] }, { $gt: ["$officialRank.point", 13] }] },
+                            1,
+                            0
+                        ]
+                    }
+                },
+                deadJCO: {
+                    $sum: {
+                        $cond: [
+                            { $and: [{ $eq: ["$status", "Dead"] }, { $lte: ["$officialRank.point", 13] }] },
+                            1,
+                            0
+                        ]
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                force: "$_id",
+                aliveOfficer: 1,
+                aliveJCO: 1,
+                deadOfficer: 1,
+                deadJCO: 1
+            }
+        }
+    ])
+    // console.log(result.length);
+    const reportData = [...result, ...previousYearResult]
+    return reportData;
 }
